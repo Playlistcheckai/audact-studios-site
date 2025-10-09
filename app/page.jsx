b49a5c73a80766730983d220e623ee6758fbe77f
Playlistcheckai <believe@playlistcheckai.com>
2025-10-09 16:57:25 -0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: 'flex',
+      height: '100vh',
+      backgroundColor: '#000',
+      color: '#00FF88',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>AudaCT Studios 🚀</h1>
+    </main>
+  );
+}
